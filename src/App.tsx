52e0c69bddd1791125202f85cae55dc2ace97c1c
abhisheksharma <abhisheksharma131098@gmail.com>
2024-06-18 20:46:57 +0530

    test mu component with appProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/greet/Greet';
-import { Application } from './components/application/application';
+import "./App.css";
+import Greet from "./components/greet/Greet";
+import { Application } from "./components/application/application";
+import { AppProviders } from "./providers/app-providers";
+import { MuiMode } from "./components/mui/mui-mode";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Greet/>
-      <Application/>
-    </div>
+    <AppProviders>
+      <div className="App">
+        {/* <Greet />
+        <Application /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
